80f55867b669117871977291a2522cd9bde794de
ʕ•́ᴥ•̀ʔっTunmise Folorunsho <tunmisef@tsuTunmises-iMac.local>
2025-07-16 19:44:10 -0500

    added basic JS - a button to add new items to the inventory

---
+++ main.js
@@ -0,0 +1,12 @@
+const inventory = document.querySelector('#inventory');
+const addButton = document.querySelector('#add-button');
+
+
+function addItem(item) {
+    const itemElement = document.createElement('div');
+    itemElement.classList.add('item');
+    itemElement.textContent = item;
+    inventory.appendChild(itemElement);
+}
+
+addButton.addEventListener("click", () => addItem("hi"));
