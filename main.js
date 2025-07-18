e5d95dda0405b7a535b784da1d4f364fa824ab33
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-07-17 22:23:20 -0500

    addeed a feature to show the details of items in the inventory

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const inventory = document.querySelector('#inventory');
 const addButton = document.querySelector('#add-button');
 const editor = document.querySelector("#item-editor");
 const form = document.querySelector("#item-form");
+const viewer = document.querySelector("#item-viewer");
+const itemInfo = document.querySelector("#item-info");
+let infoCloseButton;
 items = {}
 
 
@@ -12,7 +15,13 @@ function addItem(item) {
     itemElement.textContent = item;
     inventory.appendChild(itemElement);
     form.reset();
-}
+    itemElement.addEventListener('click', () => {
+        itemInfo.innerHTML = getInfo(item);
+        viewer.showModal();
+        infoCloseButton = document.querySelector("#close-button");
+        infoCloseButton.addEventListener("click", () => viewer.close());
+    })
+}; 
 
 addButton.addEventListener("click", () => editor.showModal());
 form.addEventListener("submit", function (event) {
@@ -20,9 +29,26 @@ form.addEventListener("submit", function (event) {
 
     const itemData = new FormData(form);
     items[itemData.get("item-name")] = {
-        quanity: itemData.get('quantity'),
+        name: itemData.get('item-name'),
+        quanity: itemData.get('item-quantity'),
     }
     
     addItem(itemData.get('item-name'));
     editor.close();
 })
+form.addEventListener("reset", () => {
+    editor.close();
+    form.reset();
+});
+
+function getInfo(item) {
+    let returnString = '<h2>Item Info</h2> ';
+    if (!items[item]) {
+        return;
+    }
+    for (const [key, value] of Object.entries(items[item])) {
+        returnString += `<h2>${key}:</h2> <p>${value}</p>`;
+    }
+    returnString += `<br> <button id="close-button">Close</button>`;
+    return returnString;
+};
