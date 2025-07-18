7965dcaf8998b78140756e414178830372ef3021
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-07-17 21:45:26 -0500

    added a basic form for the name and quanity for items using a dialog

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const inventory = document.querySelector('#inventory');
 const addButton = document.querySelector('#add-button');
+const editor = document.querySelector("#item-editor");
+const form = document.querySelector("#item-form");
+items = {}
 
 
 function addItem(item) {
+    if (!item) return;
     const itemElement = document.createElement('div');
     itemElement.classList.add('item');
     itemElement.textContent = item;
     inventory.appendChild(itemElement);
+    form.reset();
 }
 
-addButton.addEventListener("click", () => addItem(prompt("Enter item name:")));
+addButton.addEventListener("click", () => editor.showModal());
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const itemData = new FormData(form);
+    items[itemData.get("item-name")] = {
+        quanity: itemData.get('quantity'),
+    }
+    
+    addItem(itemData.get('item-name'));
+    editor.close();
+})
