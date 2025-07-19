5e4d503018c76df7d167ebaf696fcd083f6d862a
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-07-18 19:54:41 -0500

    added a feature to add new properties to items

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const editor = document.querySelector("#item-editor");
 const form = document.querySelector("#item-form");
 const viewer = document.querySelector("#item-viewer");
 const itemInfo = document.querySelector("#item-info");
-let infoCloseButton;
+let PropertyDiv = document.querySelector("#new-input");
+const Orginal_Form = document.querySelector("#item-form").innerHTML
+let newPropBtn;
+let AddPropBtn;
 items = {}
 
 
@@ -23,7 +26,12 @@ function addItem(item) {
     })
 }; 
 
-addButton.addEventListener("click", () => editor.showModal());
+addButton.addEventListener("click", () => {
+    editor.showModal()
+    document.querySelector("#item-form").innerHTML = Orginal_Form
+    newPropBtn = document.querySelector("#new-property")
+    newPropBtn.addEventListener("click", newProperty)
+});
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -53,3 +61,20 @@ function getInfo(item) {
     returnString += `<br> <button id="close-button">Close</button>`;
     return returnString;
 };
+
+
+function newProperty() {
+    PropertyDiv.innerHTML = '<label for="new">New Property:</label> <input type="text" id="new" name="new" required> <button id = "addProperty">Add</button>'
+    AddPropBtn = document.querySelector("#addProperty")
+    AddPropBtn.addEventListener("click", function (event) {
+        let NewPropValue = document.querySelector("#new").value
+        const newProp = document.querySelector("#new-properties")
+        if (!NewPropValue) {
+            PropertyDiv.innerHTML = `<button id = "new-property">New property</button>`
+            return
+        }
+        event.preventDefault()
+        document.querySelector("#new-properties").innerHTML += `<label for="${NewPropValue}:">${NewPropValue}:</label> <br> <input type="text" id="${NewPropValue}" name="${NewPropValue}"> <br> <br>`
+        PropertyDiv.innerHTML = `<button id = "new-property">New property</button>`
+    })
+} 
